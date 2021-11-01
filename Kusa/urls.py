d3918e476c00804e0f48ac38df11994c2be16ae0
Michelle Chu <michellechu@Michelles-MacBook-Air.local>
2021-11-01 15:40:08 -0700

    set up basic views structure

--- Kusa/urls.py
+++ Kusa/urls.py
@@ -1,5 +1,4 @@
 """admin URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -13,11 +12,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('Kusa.urls')),
+    path('test', views.test, name='test'),
+    path('GetGlobalAchievementPercentagesForApp/', views.GetGlobalAchievementPercentagesForApp, name='GetGlobalAchievementPercentagesForApp'),
+
 ]
--- Kusa/views.py
+++ Kusa/views.py
@@ -1,3 +1,13 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
+from admin import settings
+import requests
 
-# Create your views here.
+conf =  settings.CONF
+
+# http://api.steampowered.com/<interface name>/<method name>/v<version>/?key=<api key>&format=<format>.
+def test(request):
+    return JsonResponse({'foo':'bar'})
+
+def GetGlobalAchievementPercentagesForApp(request):
+   response = requests.get(conf["steam_api_url"]+)
