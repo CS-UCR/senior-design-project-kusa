60de41e738966e5a8b8b21c83e11e1ea7e4e6bc9
Michelle <michellechu0131@gmail.com>
2021-11-02 13:58:39 -0700

    removed commments

--- server/Kusa/iSteamUserStats.py
+++ server/Kusa/iSteamUserStats.py
@@ -29,8 +29,6 @@ def get_user_stats_for_game(request):
     method = "/GetUserStatsForGame"
     steam_id = request.GET.get("steamid")
     app_id = request.GET.get("appid")
-    print("url",conf["steam_api_url"]+ interface + method + v2 + "/?key=" + conf["steam_api_key"] + "&appid=" + app_id + "&steamid=" + steam_id + "&format=" + format)
     response = requests.get(conf["steam_api_url"]+ interface + method + v2 + "/?key=" + conf["steam_api_key"] + "&appid=" + app_id + "&steamid=" + steam_id + "&format=" + format).json()
-    print("r:",response)
     return JsonResponse(response)
 
