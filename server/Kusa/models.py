45dad3dff94e8115d241665adb2508d1f657d4b7
gabbyo6 <gabbyo6@icloud.com>
2021-11-15 21:05:28 -0800

    Begin migration to djongo

--- server/Kusa/models.py
+++ server/Kusa/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    email = models.CharField('User Email')
+    password = models.CharField('User Password')
+    date = models.DateField('Time Registered')
+    steamname = models.CharField('Steam Account Username')
+    class Meta:
+        app_label = 'Kusa'
