dc3efdc5d3b0a27b7005263939ae2bd233f0ad4d
gabbyo6 <gabbyo6@icloud.com>
2021-11-22 19:39:26 -0800

    Initial component setup for profile and library'

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -1,16 +1,43 @@
 import * as React from "react";
-import { Container, Box, Grid } from "@mui/material";
+import { Container, Grid } from "@mui/material";
 import { KusaBox } from "../../components/KusaBox/KusaBox";
+import { KusaHeader } from "../../components/KusaHeader/KusaHeader";
+import { KusaButton } from "../../components/KusaButton/KusaButton";
 
 export const Profile: React.FC = () => {
     return (
-        <Container>
-            <h1 className="section-header"> your information </h1>
-            <KusaBox width="90%" height="50%">
-                <Grid container spacing={1}>
-                    <h1>yo</h1>
+        <Container sx={{ textAlign: "left" }}>
+            <KusaHeader styles={{ marginTop: "7rem" }}>
+                your information
+            </KusaHeader>
+            <KusaBox width="100%" height="10rem">
+                <Grid container spacing={2}>
+                    <Grid item xs={4}></Grid>
                 </Grid>
             </KusaBox>
+            <KusaHeader>account links</KusaHeader>
+            <KusaBox width="100%" height="15rem">
+                <Grid container spacing={2}>
+                    <Grid item xs={4}></Grid>
+                </Grid>
+            </KusaBox>
+            <KusaHeader>account options</KusaHeader>
+            <Grid
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                spacing={2}
+            >
+                <Grid item>
+                    <KusaButton label="reset password" />
+                </Grid>
+                <Grid item>
+                    <KusaButton label="toggle emails" />
+                </Grid>
+                <Grid item>
+                    <KusaButton label="deactivate" />
+                </Grid>
+            </Grid>
         </Container>
     );
 };
