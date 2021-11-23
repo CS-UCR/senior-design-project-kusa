954146249670a9be2e843cf2fab5733d652b5662
gabbyo6 <gabbyo6@icloud.com>
2021-11-23 12:18:16 -0800

    Add assets, finish profile, add alerts

--- client/src/components/Kusa/KusaButton/KusaButton.tsx
+++ client/src/components/Kusa/KusaButton/KusaButton.tsx
@@ -1,16 +1,23 @@
 import { Button } from "@mui/material";
 
-export const KusaButton: React.FC<any> = ({ label, color, styles }) => (
+export const KusaButton: React.FC<any> = ({
+    label,
+    color,
+    styles,
+    onClick,
+}) => (
     <Button
         variant="outlined"
         color={color}
         sx={{
+            boxShadow: 1,
             borderRadius: 10,
             borderWidth: 2,
             fontSize: "1.2rem",
             textTransform: "lowercase",
             ...styles,
         }}
+        onClick={onClick}
     >
         {label}
     </Button>
