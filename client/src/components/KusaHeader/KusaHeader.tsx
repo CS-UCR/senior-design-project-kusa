0adba3b9b5bef59297ced1a2648a50cff3942ab4
gabbyo6 <gabbyo6@icloud.com>
2021-11-22 20:34:58 -0800

    WIP adding different colored sections, icons

--- client/src/components/KusaHeader/KusaHeader.tsx
+++ client/src/components/KusaHeader/KusaHeader.tsx
@@ -1,11 +1,19 @@
 import { Typography } from "@mui/material";
 
-export const KusaHeader:React.FC<any> = ({children, styles}) => {
+export const KusaHeader: React.FC<any> = ({ children, color, styles }) => {
+    const colorText = color ? color : "secondary.contrastText";
     return (
         <Typography
             variant="h4"
             component="h4"
-            sx={{ marginTop: "4rem", marginBottom: "1rem", fontWeight: "600", color: 'secondary.contrastText', ...styles}}
+            sx={{
+                marginTop: "4rem",
+                marginBottom: "1rem",
+                fontWeight: "600",
+                color: colorText,
+                verticalAlign: "bottom",
+                ...styles,
+            }}
         >
             {children}
         </Typography>
--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -1,43 +1,72 @@
 import * as React from "react";
-import { Container, Grid } from "@mui/material";
+import { Box, Container, Grid } from "@mui/material";
 import { KusaBox } from "../../components/KusaBox/KusaBox";
 import { KusaHeader } from "../../components/KusaHeader/KusaHeader";
 import { KusaButton } from "../../components/KusaButton/KusaButton";
+import { default as linkSVG } from "../../assets/profile/link.svg";
+import { default as suitcase } from "../../assets/profile/suitcase.svg";
+import "./Profile.scss";
 
 export const Profile: React.FC = () => {
     return (
-        <Container sx={{ textAlign: "left" }}>
-            <KusaHeader styles={{ marginTop: "7rem" }}>
-                your information
-            </KusaHeader>
-            <KusaBox width="100%" height="10rem">
-                <Grid container spacing={2}>
-                    <Grid item xs={4}></Grid>
-                </Grid>
-            </KusaBox>
-            <KusaHeader>account links</KusaHeader>
-            <KusaBox width="100%" height="15rem">
-                <Grid container spacing={2}>
-                    <Grid item xs={4}></Grid>
-                </Grid>
-            </KusaBox>
-            <KusaHeader>account options</KusaHeader>
-            <Grid
-                direction="column"
-                justifyContent="center"
-                alignItems="center"
-                spacing={2}
+        <>
+            <Container>
+                <KusaHeader styles={{ marginTop: "7rem" }}>
+                    <img src={suitcase} alt="suit" />
+                    your information
+                </KusaHeader>
+                <KusaBox width="100%" height="10rem">
+                    <Grid container spacing={2}>
+                        <Grid item xs={4}></Grid>
+                    </Grid>
+                </KusaBox>
+                <div className="inline"></div>
+                <KusaHeader>
+                    <img src={linkSVG} alt="link" />
+                    account links
+                </KusaHeader>
+                <KusaBox width="100%" height="15rem" styles={{marginBottom:'4rem'}}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={4}></Grid>
+                    </Grid>
+                </KusaBox>
+            </Container>
+
+            <Box
+                minWidth="100%"
+                height="100%"
+                sx={{
+                    backgroundColor: "secondary.contrastText",
+                    paddingTop: "2rem",
+                }}
             >
-                <Grid item>
-                    <KusaButton label="reset password" />
-                </Grid>
-                <Grid item>
-                    <KusaButton label="toggle emails" />
-                </Grid>
-                <Grid item>
-                    <KusaButton label="deactivate" />
-                </Grid>
-            </Grid>
-        </Container>
+                <Container>
+                    <KusaHeader color="primary.contrastText">
+                        <img src={suitcase} alt="suit" />
+                        account options
+                    </KusaHeader>
+                    <Grid
+                        direction="column"
+                        justifyContent="center"
+                        alignItems="center"
+                        spacing={2}
+                        textAlign="center"
+                    >
+                        <Grid item>
+                            <KusaButton
+                                label="reset password"
+                                color="success"
+                            />
+                        </Grid>
+                        <Grid item marginTop={2}>
+                            <KusaButton label="toggle emails" color="warning" />
+                        </Grid>
+                        <Grid item marginTop={2}>
+                            <KusaButton label="deactivate" color="error" />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Box>
+        </>
     );
 };
