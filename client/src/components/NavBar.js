de3f774963c2601c356f03b46880937841b7895b
galacticgoob <65988711+galacticgoob@users.noreply.github.com>
2021-11-22 21:59:59 -0800

    temp

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
+
 import "./NavBar.css";
-import { CSSTransition } from "react-transition-group";
 
 export default function Header() {
   const [isNavVisible, setNavVisibility] = useState(false);
@@ -24,19 +24,21 @@ export default function Header() {
     }
   };
 
-  const toggleNav = () => {
-    setNavVisibility(!isNavVisible);
-  };
-
   return (
     <header className="Header">
-      <CSSTransition
-        in={!isSmallScreen || isNavVisible}
-        timeout={350}
-        classNames="NavAnimation"
-        unmountOnExit
-      >
-        <nav className="Nav">
+      <nav className="Nav">
+      {/* <Router>
+        <Routes>
+          <Route path='/home' element={<Home/>} />
+          <Route path='/garden' element={<Garden/>} />
+          <Route path='/chat' element={<Chat/>} />
+          <Route path='/friends' element={<Friends/>} />
+          <Route path='/achievements' element={<Achievements/>} />
+          <Route path='/account' element={<Account/>} />
+          <Route path='/setting' element={<Settings/>} />
+        </Routes>
+      </Router> */}
+      
         <a href="http://localhost:3000/Home">
             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="text"/>
@@ -45,6 +47,48 @@ export default function Header() {
                 <rect x="10" y="12" width="4" height="4" />
             </svg>
         </a>
+
+        <a href="http://localhost:3000/Garden">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plant" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M7 15h10v4a2 2 0 0 1 -2 2h-6a2 2 0 0 1 -2 -2v-4z" />
+            <path d="M12 9a6 6 0 0 0 -6 -6h-3v2a6 6 0 0 0 6 6h3" />
+            <path d="M12 11a6 6 0 0 1 6 -6h3v1a6 6 0 0 1 -6 6h-3" />
+            <line x1="12" y1="15" x2="12" y2="9" />
+          </svg>
+        </a>
+        
+        <a href="http://localhost:3000/Chat">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-message" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M4 21v-13a3 3 0 0 1 3 -3h10a3 3 0 0 1 3 3v6a3 3 0 0 1 -3 3h-9l-4 4" />
+            <line x1="8" y1="9" x2="16" y2="9" />
+            <line x1="8" y1="13" x2="14" y2="13" />
+          </svg>
+        </a>
+
+        <a href="http://localhost:3000/Friends">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-friends" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <circle cx="7" cy="5" r="2" />
+            <path d="M5 22v-5l-1 -1v-4a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4l-1 1v5" />
+            <circle cx="17" cy="5" r="2" />
+            <path d="M15 22v-4h-2l2 -6a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1l2 6h-2v4" />
+          </svg>
+        </a>
+
+        <a href="http://localhost:3000/Achievements">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trophy" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <line x1="8" y1="21" x2="16" y2="21" />
+            <line x1="12" y1="17" x2="12" y2="21" />
+            <line x1="7" y1="4" x2="17" y2="4" />
+            <path d="M17 4v8a5 5 0 0 1 -10 0v-8" />
+            <circle cx="5" cy="9" r="2" />
+            <circle cx="19" cy="9" r="2" />
+          </svg>
+        </a>
+
         <a href="http://localhost:3000/Account">
             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
@@ -53,21 +97,15 @@ export default function Header() {
             </svg>
         </a>
 
-        <a href="">
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-            <line x1="4" y1="6" x2="20" y2="6" />
-            <line x1="4" y1="12" x2="20" y2="12" />
-            <line x1="4" y1="18" x2="20" y2="18" />
-        </svg>
+        <a href="http://localhost:3000/Settings">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+              <line x1="4" y1="6" x2="20" y2="6" />
+              <line x1="4" y1="12" x2="20" y2="12" />
+              <line x1="4" y1="18" x2="20" y2="18" />
+          </svg>
         </a>
-          {/* <button href="/"> Garden</button>
-          <button href="/"> Chat</button>
-          <button href="/"> Friends</button>
-          <button href="/"> Achievements</button>
-          <button href="/"> Settings</button> */}
-        </nav>
-      </CSSTransition>
+      </nav>
     </header>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -22,7 +22,7 @@ export default function AccountMenu() {
     setAnchorEl(null);
   };
   return (
-    <React.Fragment>
+    <>
       <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
         <Typography sx={{ minWidth: 100 }}>Contact</Typography>
         <Typography sx={{ minWidth: 100 }}>Profile</Typography>
@@ -92,6 +92,6 @@ export default function AccountMenu() {
           Logout
         </MenuItem>
       </Menu>
-    </React.Fragment>
+    </>
   );
 }
