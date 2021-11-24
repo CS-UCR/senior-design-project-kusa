42eac6be735b9259e04043f9a1e7b366e220bfd5
galacticgoob <65988711+galacticgoob@users.noreply.github.com>
2021-11-23 18:21:16 -0800

    Switch NavBar to router and added NavBarElements.js

--- client/src/components/KusaNavBar/NavBarElements.js
+++ client/src/components/KusaNavBar/NavBarElements.js
@@ -2,19 +2,16 @@ import { FaBars } from 'react-icons/fa';
 import { NavLink as Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-export const Nav = styled.nav
-`
-    height: 60px;
-    display: flex;
-    justify-content: space-between;
-    padding: 0.5rem calc((100vw - 1850px) / 2);
-    z-index: 10;
-`
-;
+export const Nav = styled.nav`
+  display: flex;
+  height: 60px;
+  justify-content: space-between;
+  padding: 0.5rem calc((100vw - 1850px) / 2);
+  z-index: 10;
+`;
 
-export const NavLink = styled(Link)
-`
-  color: #1e4418;
+export const NavLink = styled(Link)`
+  color: #1E4418;
   display: flex;
   align-items: center;
   text-decoration: none;
@@ -22,12 +19,11 @@ export const NavLink = styled(Link)
   height: 100%;
   cursor: pointer;
   &.active {
-    color: #ffffff;
+    color: #fff;
   }
 `;
 
-export const Bars = styled(FaBars)
-`
+export const Bars = styled(FaBars)`
   display: none;
   color: #fff;
   @media screen and (max-width: 768px) {
@@ -41,23 +37,25 @@ export const Bars = styled(FaBars)
   }
 `;
 
-export const NavMenu = styled.div
-`
+export const NavMenu = styled.div`
   display: flex;
   align-items: center;
   margin-right: -24px;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 
-
-export const NavBtn = styled.nav
-`
+export const NavBtn = styled.nav`
   display: flex;
   align-items: center;
   margin-right: 24px;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 
-export const NavBtnLink = styled(Link)
-`
+export const NavBtnLink = styled(Link)`
   border-radius: 4px;
   background: #256ce1;
   padding: 10px 22px;
@@ -67,4 +65,10 @@ export const NavBtnLink = styled(Link)
   cursor: pointer;
   transition: all 0.2s ease-in-out;
   text-decoration: none;
+  margin-left: 24px;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #fff;
+    color: #010606;
+  }
 `;
