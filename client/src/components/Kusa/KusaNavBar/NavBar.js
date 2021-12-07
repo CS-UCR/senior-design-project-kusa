9923c31e5a3106b3c8b0c2f088d899f314cff8b8
galacticgoob <65988711+galacticgoob@users.noreply.github.com>
2021-12-06 19:08:41 -0800

    changed nav bar color to work with light and dark theme

--- client/src/components/Kusa/KusaNavBar/NavBar.js
+++ client/src/components/Kusa/KusaNavBar/NavBar.js
@@ -9,20 +9,11 @@ const Navbar = () => {
         <NavLink to='/'></NavLink>
         <Bars />
         <NavMenu>
-          {/* //gonna use this to implement the night mode 
-          <NavLink to='/' activeStyle>
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-flare" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-              <circle cx="12" cy="12" r="2" />
-              <path d="M3 12h4m5 -9v4m5 5h4m-9 5v4m-4.5 -13.5l1 1m8 -1l-1 1m0 7l1 1m-8 -1l-1 1" />
-            </svg>
-          </NavLink> */}          
-          <NavLink to='/' activeStyle>
-          </NavLink>
+          <NavLink to='/' activeStyle></NavLink>
           
           <DarkToggle/>
 
-          <NavLink to='/home' activeStyle>
+          <NavLink to='/home' >
             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path stroke="none" d="M0 0h24v24H0z" fill="text"/>
               <polyline points="5 12 3 12 12 3 21 12 19 12" />
@@ -89,11 +80,6 @@ const Navbar = () => {
             </svg>
           </NavLink>
         </NavMenu>
-        {/*
-        // could use to implement sign in, ignore for now
-        <NavBtn>
-          <NavBtnLink to='/signin'>Sign In</NavBtnLink>
-        </NavBtn> */}
       </Nav>
     </>
   );
