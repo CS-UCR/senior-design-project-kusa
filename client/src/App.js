3884d64fa39f292e0230fc7b7b85d76103e2b992
gabbyo6 <gabbyo6@icloud.com>
2022-01-21 15:31:39 -0800

    Route overhaul

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,19 @@ import { Home } from "./pages/Home/Home.tsx";
 import { Profile } from "./pages/Profile/Profile";
 import { FriendsList } from "./pages/FriendsList/FriendsList";
 import { Landing } from "./pages/Landing/Landing.tsx";
-import { PrivateRoute } from "./routes/PrivateRoute/PrivateRoute";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import {
+    BrowserRouter,
+    Route,
+    Routes,
+    useLocation,
+    Navigate,
+} from "react-router-dom";
 import { CustomThemeProvider } from "./contexts/ThemeContext/ThemeContext";
-import { UserContextProvider } from "./contexts/UserContext/UserContext";
+import {
+    UserContext,
+    UserContextProvider,
+} from "./contexts/UserContext/UserContext";
 import { GlobalStyles } from "@mui/material";
 import { CSSTransition } from "react-transition-group";
 import { Login } from "./pages/Login/Login";
@@ -16,28 +24,59 @@ import { Signup } from "./pages/Signup/Signup";
 import "./App.css";
 import "animate.css";
 
-/* 
-<Route path="/garden" element={Garden} />
-<Route path="/chat" element={Chat} />
-<Route path="/friends" element={Friends} />           
-<Route path="/achievements" element={Achievements} />
-<Route path="/account" element={Account} />
-<Route path="/settings" element={Settings} /> 
-*/
-
-const routes = [
-    { path: "/", name: "Landing", Component: Landing },
-    //public routes go here
-    { path: "/login", name: "Home", Component: Login },
-    { path: "/signup", name: "Profile", Component: Signup },
-    //private routes go here
-    { path: "/", name: "Landing", Component: PrivateRoute },
-    { path: "/home", name: "Home", Component: Home },
-    { path: "/profile", name: "Profile", Component: Profile },
-    { path: "/friends", name: "FriendsList", Component: FriendsList },
-];
-
 function App() {
+    const { isLoggedIn } = React.useContext(UserContext);
+
+    const privateRoute = (Component) =>
+        isLoggedIn ? Component : <Navigate to="?" />;
+
+    const routes = [
+        { path: "/", name: "Landing", Component: Landing },
+        //public routes go here
+        { path: "/login", name: "Home", Component: Login },
+        { path: "/signup", name: "Profile", Component: Signup },
+        //private routes go here
+        {
+            path: "/home",
+            name: "Home",
+            Component: privateRoute(Home),
+        },
+        {
+            path: "/profile",
+            name: "Profile",
+            Component: privateRoute(Profile),
+        },
+        { path: "/friends", name: "FriendsList", Component: FriendsList },
+    ];
+
+    const AnimatedApp = React.memo(() => {
+        const location = useLocation();
+        return (
+            <Routes>
+                {routes.map(({ path, name, Component }) => {
+                    return (
+                        <Route
+                            key={name}
+                            exact
+                            path={path}
+                            render={() => (
+                                <CSSTransition
+                                    appear
+                                    in={location.pathname === path}
+                                    key={name}
+                                    classNames="screen"
+                                    timeout={100}
+                                >
+                                    <Component />
+                                </CSSTransition>
+                            )}
+                        />
+                    );
+                })}
+            </Routes>
+        );
+    });
+
     return (
         <div className="App">
             <GlobalStyles
@@ -69,33 +108,6 @@ const ComposeApp = () => {
 };
 
 //revisit - probably not performant to be rerendering the routes on new location change
-const AnimatedApp = React.memo(() => {
-    const location = useLocation();
-    return (
-        <Routes>
-            {routes.map(({ path, name, Component }) => {
-                return (
-                    <Route
-                        key={name}
-                        exact
-                        path={path}
-                        element={
-                            <CSSTransition
-                                appear
-                                in={location.pathname === path}
-                                key={name}
-                                classNames="screen"
-                                timeout={100}
-                            >
-                                <Component />
-                            </CSSTransition>
-                        }
-                    />
-                );
-            })}
-        </Routes>
-    );
-});
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
--- client/src/routes/PrivateRoute/PrivateRoute.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { Outlet, Navigate, RouteProps } from "react-router";
-import { UserContext } from "../../contexts/UserContext/UserContext";
-
-export const PrivateRoute: React.FC<RouteProps> = () => {
-    const {isLoggedIn} = React.useContext(UserContext);
-    return !isLoggedIn ? <Navigate to="/" /> : <Outlet />;
-};
