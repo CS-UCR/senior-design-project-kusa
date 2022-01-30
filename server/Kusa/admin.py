df323a779b948782d92a8ae6e093f1759c3a0cae
galacticgoob <65988711+galacticgoob@users.noreply.github.com>
2022-01-29 19:08:50 -0800

    Created FriendsList and FriendRequest models, added FriendsListAdmin and FriendRequestAdmin

--- server/Kusa/admin.py
+++ server/Kusa/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Gamer
+from Kusa.models import FriendsList, FriendRequest
 
 # Register your models here.
 
@@ -16,4 +17,21 @@ class GamerAdmin(admin.ModelAdmin):
 
 admin.site.register(Gamer, GamerAdmin)    
 
+class FriendsListAdmin(admin.ModelAdmin):
+    list_filter = ['Gamer']
+    list_display = ['Gamer']
+    search_fields = ['Gamer']
+    readonly_fields = ['Gamer']
 
+    class Meta:
+        model = FriendsList
+admin.site.register(FriendsList, FriendsListAdmin)
+
+class FriendRequestAdmin(admin.ModelAdmin):
+    list_filter = ['sender', 'receiver']
+    list_display = ['sender', 'receiver']
+    search_fields = ['sender__UserID', 'sender__Email', 'receiver', 'receiver__email']
+
+    class Meta:
+        model = FriendRequest
+admin.site.register(FriendRequest, FriendRequestAdmin)
--- server/Kusa/models.py
+++ server/Kusa/models.py
@@ -2,6 +2,9 @@ from email.policy import default
 from django.forms import CharField
 from django.contrib.postgres.fields import ArrayField
 from djongo import models
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
 # Create your models here.
 
 
@@ -34,3 +37,61 @@ class Test(models.Model):
 
     # def _str_(self):
     #     return self.Name
+
+class FriendsList(models.Model):
+    Gamer = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="Gamer") #OneToOne = one friendslist per user
+    Friends = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name="Friends") #List of users
+    def _str_(self):
+        return self.Gamer.UserID
+
+    def add_friend(self, account): # add new friend 
+        if not account in self.Friends.all():
+            self.Friends.add(account)
+            self.save()
+        
+    def remove_friend(self, account):   # remove friend
+        if account in self.Friends.all():
+            self.Friends.remove(account)
+            self.save()
+
+    def unfriend(self, removee):    # start unfriending process
+        remover_friends_list = self # person starting the removal
+        remover_friends_list.remove_friend(removee)
+
+        #remove friend from removee friend list
+        friends_list = FriendsList.objects.get(user=removee)
+        friends_list.remove_friend(self.user)
+
+    def are_friends(self, friend):  # check if user are friends
+        if friend in self.firends.all():
+            return True
+        return False    
+
+class FriendRequest(models.Model):
+    # Sender
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="sender")
+    # Receiver
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="receiver")
+    is_pending = models.BooleanField(blank = True, null = False, default = True) # when true, request is pending
+    timestamp = models.DateTimeField(auto_now_add = True)
+    def _str_(self):
+        return self.sender.UserID
+
+    def accept(self):  # accept a friend request, then update sender and receiver friendslists
+        receiver_friendslist = FriendsList.objects.get(Gamer=self.receiver)
+        if receiver_friendslist:
+            receiver_friendslist.add_friend(self.sender)
+            sender_friendslist = FriendsList.objects.get(Gamer=self.sender)
+            if sender_friendslist:
+                sender_friendslist.add_friend(self.receiver)
+                self.is_pending = False
+                self.save()
+
+    def decline(self): # decline a friend request by setting is_pending to false
+        self.is_pending = False
+        self.save()
+
+    def cancel(self):# cancel a friend request by setting is_pending to false (for notifications)
+        self.is_pending = False
+        self.save()
+        
