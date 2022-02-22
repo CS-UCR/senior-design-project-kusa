7ff6eb4222a324777e3bd514b92b21e86dab1b35
galacticgoob <65988711+galacticgoob@users.noreply.github.com>
2022-02-22 02:33:56 -0800

    made comments on conversations

--- server/Kusa/conversations.py
+++ server/Kusa/conversations.py
@@ -12,13 +12,20 @@ from django.views.decorators.csrf import csrf_exempt
 def newConversation(request, senderID, self):
     #newConversation = Conversation
         #members:[req.body.senderID, req.body.receiverID]
-    newConversation = Conversation(senderID=request.POST.get("senderID"), receiverID=request.POST.get("SteamID"))
+    newConversation = Conversation("members").split(",")
+    #newConversation = Conversation(senderID=request.POST.get("senderID"), receiverID=request.POST.get("SteamID"))
     if savedConversation == newConversation.save():
         return JsonResponse(savedConversation, status=200)
+
     else:
         return JsonResponse(conversation_serializer.errors, status=500)
 
 # get convo of a user
+    # try
+        # convo.filter(in members userID)
+        # return json(convo) 
+    # catch
+        # return json(error)
 
 
 
