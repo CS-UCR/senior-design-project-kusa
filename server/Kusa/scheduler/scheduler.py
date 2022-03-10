80d80af5b12047e23d1bb040ec6b1cce47c093cc
gabbyo6 <gabbyo6@icloud.com>
2022-03-09 18:29:34 -0800

    Setup and add emails to scheduler

--- server/Kusa/scheduler/scheduler.py
+++ server/Kusa/scheduler/scheduler.py
@@ -6,29 +6,39 @@ from django_apscheduler.jobstores import DjangoJobStore
 from time import gmtime, strftime
 from collections import OrderedDict
 from Kusa.data_collection import get_steam_user
+from Kusa.views import send_user_email
+
 
 def has_duplicate_entry(date, array):
     for json in array:
         if json["date"] == date:
-            return True 
+            return True
     return False
 
+
 def update_all_users_playtime():
-    steamuser_serializer = SteamUserSerializer(SteamUser.objects.all(), many=True)
+    steamuser_serializer = SteamUserSerializer(
+        SteamUser.objects.all(), many=True)
     date = strftime("%m/%d/%Y", gmtime())
     for entry in steamuser_serializer.data:
+        # gather and submit playtime if user has no entry for current date
         steam_id = entry['id']
         daily_hours = get_steam_user(steam_id)['daily_hours']
-        list_of_json = [dict(day) for day in eval(daily_hours)] 
-        if not has_duplicate_entry(date,list_of_json):
-            entry.daily_hours.append({'date': date, 'hours': get_total_playtime_hours(steam_id)})
+        list_of_json = [dict(day) for day in eval(daily_hours)]
+        if not has_duplicate_entry(date, list_of_json):
+            entry['daily_hours'].update(
+                {'date': date, 'hours': get_total_playtime_hours(steam_id)})
             entry.save()
-    
-    
+        # send email if sum of hours is over goal for current week
+        if(get_steam_user(steam_id)['emailsEnabled']):
+            send_user_email(steam_id)
+      
+
+
 def start():
     scheduler = BackgroundScheduler()
     scheduler.add_jobstore(DjangoJobStore(), "default")
-    # run this job once every 24 hrs 
-    scheduler.add_job(update_all_users_playtime, 'interval', hours=24, id='update_all_users_playtime', misfire_grace_time=None)
+    # run this job once every 24 hrs
+    scheduler.add_job(update_all_users_playtime, 'interval', minutes=1,
+                      id='update_all_users_playtime', misfire_grace_time=None)
     scheduler.start()
-    
--- server/Kusa/views.py
+++ server/Kusa/views.py
@@ -1,3 +1,5 @@
+from smtplib import SMTPException
+from django.http import BadHeaderError
 from django.http.response import JsonResponse
 from django.shortcuts import redirect
 from admin import settings
@@ -5,10 +7,12 @@ from admin.settings import FRONTEND_URL
 from Kusa.authentication import get_token
 from Kusa.authentication import validate_token
 from Kusa.data_collection import get_steam_user
-from collections import OrderedDict #keep this line for get_user_daily_hours 
+from collections import OrderedDict  # keep this line for get_user_daily_hours
+from datetime import datetime
+from django.core.mail import send_mail
 
 JWT_SECRET_KEY = settings.JWT_SECRET_KEY
-conf =  settings.CONF
+conf = settings.CONF
 
 # http://api.steampowered.com/<interface name>/<method name>/v<version>/?key=<api key>&format=<format>.
 
@@ -28,11 +32,30 @@ def get_user_daily_hours(request):
     Returns: returns a list of json obj -> [{"date" : date1, "hours" : num_hours1},{"date" : date2, "hours" : num_hours2}]
     """
     response = validate_token(request)
-    if "steamid" in response:   
-        user =  get_steam_user(response["steamid"])
+    if "steamid" in response:
+        user = get_steam_user(response["steamid"])
         daily_hours = user['daily_hours']
         list_of_json = [dict(day) for day in eval(daily_hours)]
-        return JsonResponse(list_of_json , safe=False)
-    else: 
+        return JsonResponse(list_of_json, safe=False)
+    else:
         return response
-    
+
+
+def send_user_email(steam_id):
+    success = False
+    user = get_steam_user(steam_id)
+    daily_hours = user['daily_hours']
+    goal = user['goal']
+    list_of_json = [dict(day) for day in eval(daily_hours)]
+    sum = 0
+    for value in list_of_json:
+        if(datetime.today().isocalendar()[1] == datetime.strptime(value['date'], "%m/%d/%Y").isocalendar()[1]):
+            sum += value['hours']
+        if(sum > goal):
+            print("sum is greater than goal")
+            try:
+                send_mail("Kusa Playtime Exceeded", 'You exceeded your goal for this week! Better luck next time. Remember, you can change your goal in the Kusa app.', settings.EMAIL_HOST_USER, [user['email']], fail_silently=False)
+                success = True
+            except SMTPException as e:
+                print(e)
+    return JsonResponse({'success': success}, safe=False)
