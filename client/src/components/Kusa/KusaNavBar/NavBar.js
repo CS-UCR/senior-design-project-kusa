5333b59f0864cc681355f57c6e1c5946df251745
gabbyo6 <gabbyo6@icloud.com>
2022-03-12 14:56:49 -0800

    Navbar edit

--- client/src/components/Kusa/KusaNavBar/NavBar.js
+++ client/src/components/Kusa/KusaNavBar/NavBar.js
@@ -22,16 +22,6 @@ const Navbar = () => {
             </svg>
           </NavLink>
 
-          <NavLink to='/garden' activeStyle>
-            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-plant" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-              <path d="M7 15h10v4a2 2 0 0 1 -2 2h-6a2 2 0 0 1 -2 -2v-4z" />
-              <path d="M12 9a6 6 0 0 0 -6 -6h-3v2a6 6 0 0 0 6 6h3" />
-              <path d="M12 11a6 6 0 0 1 6 -6h3v1a6 6 0 0 1 -6 6h-3" />
-              <line x1="12" y1="15" x2="12" y2="9" />
-            </svg>
-          </NavLink>
-
           <NavLink to='/chat' activeStyle>
             <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-message" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
@@ -68,15 +58,6 @@ const Navbar = () => {
               <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
               <circle cx="12" cy="7" r="4" />
               <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-            </svg>
-          </NavLink>
-
-          <NavLink to='/settings' activeStyle>
-            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-menu-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                <line x1="4" y1="6" x2="20" y2="6" />
-                <line x1="4" y1="12" x2="20" y2="12" />
-                <line x1="4" y1="18" x2="20" y2="18" />
             </svg>
           </NavLink>
         </NavMenu>
