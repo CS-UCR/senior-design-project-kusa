3e456f4b808768646b0bd623b4a5c32859ac17d4
galacticgoob <65988711+galacticgoob@users.noreply.github.com>
2022-03-12 22:01:27 -0800

    fixed invites ui

--- client/src/components/FriendsList/FriendsListField/FriendsListField.tsx
+++ client/src/components/FriendsList/FriendsListField/FriendsListField.tsx
@@ -15,8 +15,8 @@ import {
     DialogContentText,
     DialogTitle,
     TextField,
-    
-    
+
+
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -43,38 +43,35 @@ const friendImg = 2;
 
 
 
-export const FriendsListField: React.FC<any> = ({ friendInfo, friendList,setFriendList, userId}) =>
-{
+export const FriendsListField: React.FC<any> = ({ friendInfo, friendList, setFriendList, userId }) => {
 
-    
-    
+
+
     const handleDelete = () => {
         axios.get(`${BACKEND_URL}/deleteFriend/` + userId + "&" + friendInfo[friendSteamid]).then((response) => {
-          });
-        
+        });
 
-        for(var i = 0; i < friendList.length; i++)
-        {
-            if(friendList[i][friendName] === friendInfo[friendName])
-            {
-                friendList.splice(i,1);
+
+        for (var i = 0; i < friendList.length; i++) {
+            if (friendList[i][friendName] === friendInfo[friendName]) {
+                friendList.splice(i, 1);
                 var newFriendList = friendList
             }
         }
-        var newFriendList = friendList.filter(function(e: string) {return e !== friendInfo[friendName] })
+        var newFriendList = friendList.filter(function (e: string) { return e !== friendInfo[friendName] })
         setFriendList(newFriendList)
-        
+
     }
-    
+
     const handleChat = () => {
         axios.get(`${BACKEND_URL}/addConversation/` + userId + "&" + friendInfo[friendSteamid]).then((response) => {
         });
     }
-        
-    
-    
 
-    return(
+
+
+
+    return (
         <><KusaBox width="90%" styles={{ padding: "2rem" }}>
             <Grid container spacing={1} alignContent="center" marginTop="-10px" marginBottom="-10px">
                 <Grid item xs={1}>
@@ -84,11 +81,11 @@ export const FriendsListField: React.FC<any> = ({ friendInfo, friendList,setFrie
                         width={iconHeight}
                         height={iconHeight}
                         style={{ marginRight: "1rem", marginTop: "-0.5rem", marginBottom: "-0.5rem", borderRadius: '50%', }} />
-                        
+
                 </Grid>
                 <Grid item xs={1} />
 
-                <Grid item xs={4} > 
+                <Grid item xs={4} >
                     <KusaHeader styles={{
                         marginTop: "0.1rem",
                         marginBottom: "-0.1rem",
@@ -101,30 +98,23 @@ export const FriendsListField: React.FC<any> = ({ friendInfo, friendList,setFrie
                     }}>
                         {friendInfo[friendName]}
                     </KusaHeader>
-                        
-                    
-                    
-                    
-                        
-
-                    
                 </Grid>
 
                 <Grid item xs={2}></Grid>
 
-                <Link to="/chat" style={{ textDecoration: 'none'}}>
-                    <Button variant="contained" size="large"  color="primary" onClick={handleChat} style={{fontSize:12, fontWeight:"bold", maxWidth: '50px', minHeight: '37px', maxHeight:'37px', marginTop: "0.5rem", marginBottom: "0.5rem"}} >
+                <Link to="/chat" style={{ textDecoration: 'none' }}>
+                    <Button variant="contained" size="large" color="primary" onClick={handleChat} style={{ fontSize: 12, fontWeight: "bold", maxWidth: '50px', minHeight: '37px', maxHeight: '37px', marginTop: "0.5rem", marginBottom: "0.5rem" }} >
                         Chat
                     </Button>
                 </Link>
 
                 <Grid item xs={1}></Grid>
 
-                <Button variant="contained" color="primary" size="large" onClick={handleDelete} style={{fontSize:12, fontWeight:"bold", maxWidth: '150px', maxHeight: '37px', minHeight: '37px', marginTop: "0.5rem", marginBottom: "0.5rem"}}>
+                <Button variant="contained" color="primary" size="large" onClick={handleDelete} style={{ fontSize: 12, fontWeight: "bold", maxWidth: '150px', maxHeight: '37px', minHeight: '37px', marginTop: "0.5rem", marginBottom: "0.5rem" }}>
                     Remove Friend
                 </Button>
 
-               
+
 
             </Grid>
 
--- client/src/components/FriendsList/FriendsListField/InviteListField.tsx
+++ client/src/components/FriendsList/FriendsListField/InviteListField.tsx
@@ -15,8 +15,8 @@ import {
     DialogContentText,
     DialogTitle,
     TextField,
-    
-    
+
+
 } from "@mui/material";
 
 import { KusaBox } from "../../Kusa/KusaBox/KusaBox";
@@ -25,8 +25,9 @@ import { default as dog } from "../../../assets/friends/dog.svg"; //come back la
 import { BACKEND_URL } from "../../../constants/backendURL";
 import { KusaWhiteSpace } from "./WhiteSpace";
 import axios from "axios";
+import { KusaHeader } from "../../Kusa/KusaHeader/KusaHeader";
 
-const iconHeight = 40;
+const iconHeight = 60;
 
 const personSteamid = 0;
 const personName = 1;
@@ -42,32 +43,30 @@ export const InviteListField: React.FC<any> = ({ personInfo, inviteList, setInvi
         //pass name to the backend
         axios.get(`${BACKEND_URL}/acceptFriendRequest/` + userId + "&" + personInfo[personSteamid]).then((response) => {
             //console.log(response.data)
-          });
-        
-        var newFriend;  
+        });
+
+        var newFriend;
 
         //remove person from invitelist
-        for(var i = 0; i < inviteList.length; i++)
-        {
+        for (var i = 0; i < inviteList.length; i++) {
             console.log(inviteList)
-            if(inviteList[i][personName] === personInfo[personName])
-            {
+            if (inviteList[i][personName] === personInfo[personName]) {
                 var newFriend = inviteList[i]
-                inviteList.splice(i,1);
+                inviteList.splice(i, 1);
                 var newInviteList = inviteList
-                
+
             }
         }
 
-        var newInviteList = inviteList.filter(function(e: string) {return e !== personInfo[personName] })
+        var newInviteList = inviteList.filter(function (e: string) { return e !== personInfo[personName] })
         setInviteList(newInviteList)
-        
+
         //add person to friendlist
-        
+
         friendList.push(newFriend)
         setFriendList(friendList)
-        
-          
+
+
     };
 
 
@@ -75,18 +74,16 @@ export const InviteListField: React.FC<any> = ({ personInfo, inviteList, setInvi
         //pass name to the backend
         axios.get(`${BACKEND_URL}/rejectFriendRequest/` + userId + "&" + personInfo[personSteamid]).then((response) => {
             //console.log(response.data)
-          });
-        
-        for(var i = 0; i < inviteList.length; i++)
-        {
-            if(inviteList[i][personName] === personInfo[personName])
-            {
-                inviteList.splice(i,1);
+        });
+
+        for (var i = 0; i < inviteList.length; i++) {
+            if (inviteList[i][personName] === personInfo[personName]) {
+                inviteList.splice(i, 1);
                 var newInviteList = inviteList
             }
         }
-        
-        var newInviteList = inviteList.filter(function(e: string) {return e !== personInfo[personName] })
+
+        var newInviteList = inviteList.filter(function (e: string) { return e !== personInfo[personName] })
         setInviteList(newInviteList)
 
     };
@@ -96,40 +93,46 @@ export const InviteListField: React.FC<any> = ({ personInfo, inviteList, setInvi
 
 
 
-    return(
+    return (
 
         <><KusaBox width="90%" styles={{ padding: "2rem" }}>
-            <Grid container spacing={1}>
+            <Grid container spacing={1} alignContent="center" marginTop="-10px" marginBottom="-10px">
                 <Grid item xs={1}>
                     <img
                         src={personInfo[personImg]}
                         alt="invite"
                         width={iconHeight}
                         height={iconHeight}
-                        style={{ marginRight: "1rem", marginBottom: "-0.5rem" }} />
+                        style={{ marginRight: "1rem", marginTop: "-0.5rem", marginBottom: "-0.5rem", borderRadius: '50%', }} />
                 </Grid>
                 <Grid item xs={1} />
 
-                <Grid item xs={1}>
-
-                    {personInfo[personName]}
+                <Grid item xs={4} >
+                    <KusaHeader styles={{
+                        marginTop: "0.1rem",
+                        marginBottom: "-0.1rem",
+                        marginLeft: "-5rem",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "left",
+                        fontSize: "30px",
+                        fontWeight: "500",
+                    }}>
+                        {personInfo[personName]}
+                    </KusaHeader>
                 </Grid>
-
-                <Grid item xs={4}></Grid>
-
-                <Button variant="contained" color="success" size="small" onClick={handleAccept}>
-                    Accept
-                </Button>
 
                 <Grid item xs={2}></Grid>
 
-                <Button variant="contained" color="success" size="small" onClick={handleReject}>
-                    Reject
+                <Button variant="contained" size="large" color="primary" onClick={handleAccept} style={{ fontSize: 12, fontWeight: "bold", maxWidth: '50px', minHeight: '37px', maxHeight: '37px', marginTop: "0.5rem", marginBottom: "0.5rem" }} >
+                    Accept
                 </Button>
 
-                <Grid item xs={2}>
+                <Grid item xs={1}></Grid>
 
-                </Grid>
+                <Button variant="contained" color="primary" size="large" onClick={handleReject} style={{ fontSize: 12, fontWeight: "bold", maxWidth: '150px', maxHeight: '37px', minHeight: '37px', marginTop: "0.5rem", marginBottom: "0.5rem" }}>
+                    Reject
+                </Button>
 
             </Grid>
 
@@ -141,4 +144,4 @@ export const InviteListField: React.FC<any> = ({ personInfo, inviteList, setInvi
 
 
 }
-    
+
