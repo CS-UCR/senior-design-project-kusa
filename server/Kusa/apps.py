b36993cc42b1d0c721d52422b57e6d46bbb517b7
Michelle Chu <michellechu0131@gmail.com>
2022-03-13 18:32:39 -0700

    Added friend achievements

--- server/Kusa/apps.py
+++ server/Kusa/apps.py
@@ -5,5 +5,5 @@ class KusaConfig(AppConfig):
     name = 'Kusa'
     def ready(self):
             from Kusa.scheduler import scheduler
-            scheduler.start()
+            # scheduler.start()
     
